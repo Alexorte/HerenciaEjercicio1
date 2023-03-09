28a6e068704265826af274a8152e24e94fe313ca
Alex <alex.ortega@alu.uclm.es>
2023-03-09 11:15:36 +0100

    Finalizacion de abogado

--- src/Abogado.java
+++ src/Abogado.java
@@ -7,4 +7,16 @@ public class Abogado extends Empleado{
     public double getSalario(){
         return super.getSalario()+10000;
     }
+    @Override
+    public int getHoras() {
+        return super.getHoras()+5;
+    }
+    @Override
+    public String getMesVacaciones() {
+        return "julio";
+}
+
+    public void mostrar(){
+        System.out.println("estoy haciendo fotocopias");
+    }
 }
